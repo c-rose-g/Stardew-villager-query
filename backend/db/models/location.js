1c789054b8e339bd1dee84c3a752d00d7090703e
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-06 20:05:19 -0700

    Update location model

--- backend/db/models/location.js
+++ backend/db/models/location.js
@@ -1,35 +1,40 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Location extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      Location.hasMany(models.Building,{foreignKey:'locationId'});
-      Location.hasMany(models.Schedule,{foreignKey:'locationId'});
-      Location.hasMany(models.House, {foreignKey:'locationId'});
-    }
-  }
-  Location.init({
-    id:{
-      type:DataTypes.INTEGER,
-      primaryKey:true,
-      autoIncrement:true,
-      allowNull:false
-    },
-    name: {
-      type:DataTypes.STRING,
-      allowNull:false
-    }
-  }, {
-    sequelize,
-    modelName: 'Location',
-  });
-  return Location;
+	class Location extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			Location.hasMany(models.Building, { foreignKey: "locationId" });
+			Location.hasMany(models.Schedule, { foreignKey: "locationId" });
+			Location.hasMany(models.House, { foreignKey: "locationId" });
+			// Many-to-Many relationship with Villager
+			Location.belongsToMany(models.Villager, {
+				through: "Villager_Locations",
+			});
+		}
+	}
+	Location.init(
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+		},
+		{
+			sequelize,
+			modelName: "Location",
+		}
+	);
+	return Location;
 };

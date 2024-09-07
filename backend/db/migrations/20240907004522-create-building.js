96c05ad435a694bcd2340ee90a578f7c1a88c026
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-07 00:48:20 -0700

    Remove auto increment from Building table

--- backend/db/migrations/20240907004522-create-building.js
+++ backend/db/migrations/20240907004522-create-building.js
@@ -5,7 +5,6 @@ module.exports = {
     await queryInterface.createTable('Buildings', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },

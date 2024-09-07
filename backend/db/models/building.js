d0fb036e134694f4e0e856e18252d89bd132b8fd
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-06 17:56:08 -0700

    Create Building table

---
+++ backend/db/models/building.js
@@ -0,0 +1,41 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+	class Building extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+      Building.belongsTo(models.Location,{foreignKey:'locationId'});
+		}
+	}
+	Building.init(
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			locationId: {
+				type: DataTypes.INTEGER,
+				references: {
+					key: "id",
+					model: "Locations",
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: "Building",
+		}
+	);
+	return Building;
+};

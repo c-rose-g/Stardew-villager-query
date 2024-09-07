636197657c4a22478b361109501846b254b4ad0c
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-06 17:55:00 -0700

    Create Location table

---
+++ backend/db/models/location.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Location extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Location.hasMany(models.Building,{foreignKey:'locationId'});
+      Location.hasMany(models.Schedule,{foreignKey:'locationId'});
+    }
+  }
+  Location.init({
+    id:{
+      type:DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement:true,
+      allowNull:false
+    },
+    name: {
+      type:DataTypes.STRING,
+      allowNull:false
+    }
+  }, {
+    sequelize,
+    modelName: 'Location',
+  });
+  return Location;
+};

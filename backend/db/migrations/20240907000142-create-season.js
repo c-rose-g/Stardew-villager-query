979088e63afebba62d857f552b8910056783473d
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-06 18:03:12 -0700

    Remove ENUM conditional logic and changed name to string with validations

--- backend/db/migrations/20240907000142-create-season.js
+++ backend/db/migrations/20240907000142-create-season.js
@@ -2,8 +2,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // Check if we're using SQLite or a database that supports ENUM
-		const isSQLite = queryInterface.sequelize.options.dialect === 'sqlite';
 
     await queryInterface.createTable('Seasons', {
       id: {
@@ -13,7 +11,10 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: isSQLite ? Sequelize.STRING : Sequelize.ENUM('Spring','Summer','Fall','Winter','Year-Round'),  // Conditional ENUM
+        type:Sequelize.STRING,
+        validate:{
+          isIn:[["Spring", "Summer", "Fall", "Winter", "Year-Round"]]
+        },
         allowNull:false
       },
       createdAt: {
@@ -30,9 +31,5 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Seasons');
-    // Drop the ENUM type only if the database is not SQLite
-    if (queryInterface.sequelize.options.dialect !== 'sqlite') {
-			await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_Seasons_name";');
-		}
   }
 };

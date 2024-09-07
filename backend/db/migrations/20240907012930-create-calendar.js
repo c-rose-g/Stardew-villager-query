edfad38924d1470b656b7c3ef2848fd4963e5778
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-06 18:29:47 -0700

    Create Calendar table

---
+++ backend/db/migrations/20240907012930-create-calendar.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Calendars', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      villagerBirthdayId: {
+        type: Sequelize.INTEGER
+      },
+      seasonId: {
+        type: Sequelize.INTEGER
+      },
+      dayOfTheSeason: {
+        type: Sequelize.INTEGER
+      },
+      eventName: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Calendars');
+  }
+};

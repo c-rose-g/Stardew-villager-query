2429efe15e61d1db3589c16e31bccf211e0705be
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-07 00:47:42 -0700

    Remove auto increment from Category table

--- backend/db/migrations/20240907003317-create-category.js
+++ backend/db/migrations/20240907003317-create-category.js
@@ -5,7 +5,6 @@ module.exports = {
     await queryInterface.createTable('Categories', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },

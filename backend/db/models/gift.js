fc8038738cd307bae29a7138fcf559c5c0c55f69
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-06 19:38:38 -0700

    update Gift model

--- backend/db/models/gift.js
+++ backend/db/models/gift.js
@@ -9,7 +9,10 @@ module.exports = (sequelize, DataTypes) => {
 		 */
 		static associate(models) {
 			// define association here
-      Gift.belongsTo(models.Category,{foreignKey:'categoryId'})
+      Gift.belongsTo(models.Category,{foreignKey:'categoryId'});
+			// Many-to-Many relationship with Villager
+			Gift.belongsToMany(models.Villager, { through: 'Villager_Preferences' });
+
 		}
 	}
 	Gift.init(

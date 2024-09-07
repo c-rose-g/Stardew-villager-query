13707f6b622beea9d93f816d72e7f8d6e26c0405
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-07 00:47:19 -0700

    Remove auto increment from Gift table

--- backend/db/migrations/20240907001033-create-gift.js
+++ backend/db/migrations/20240907001033-create-gift.js
@@ -5,7 +5,6 @@ module.exports = {
 		await queryInterface.createTable("Gifts", {
 			id: {
 				allowNull: false,
-				autoIncrement: true,
 				primaryKey: true,
 				type: Sequelize.INTEGER,
 			},

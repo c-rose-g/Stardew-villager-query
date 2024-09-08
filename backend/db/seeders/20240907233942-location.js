6e800da009f4af724d9fe8df0392c7bd79e216e5
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-07 17:41:02 -0700

    Update Location seeders

--- backend/db/seeders/20240907233942-location.js
+++ backend/db/seeders/20240907233942-location.js
@@ -1,30 +1,36 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     * later - add differentiation between the valley and in beyond the valley locations
-    */
-    return queryInterface.bulkInsert('Locations',[
-      {name:'Pelican Town'},{name:'Cindersap Forest'},{name:}
-    ])
-  },
+	async up(queryInterface, Sequelize) {
+		/**
+		 * Add seed commands here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkInsert('People', [{
+		 *   name: 'John Doe',
+		 *   isBetaMember: false
+		 * }], {});
+		 * later - add differentiation between the valley and in beyond the valley locations
+     * only including standard farm
+		 */
+		return queryInterface.bulkInsert("Locations", [
+			{ name: "Pelican Town" },
+			{ name: "Cindersap Forest" },
+			{ name: "Beach" },
+			{ name: "Mountain" },
+			{ name: "Witch's Swamp" },
+      {name:'Farm'},
+		]);
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    return queryInterface.bulkDelete('Locations', null)
-  }
+	async down(queryInterface, Sequelize) {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
+		return queryInterface.bulkDelete("Locations", null);
+	},
 };

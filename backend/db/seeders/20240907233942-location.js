672f4c4ac179d086997e61e5cc5bf28cd10b8850
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-07 17:10:35 -0700

    Update Location seeders

--- backend/db/seeders/20240907233942-location.js
+++ backend/db/seeders/20240907233942-location.js
@@ -11,7 +11,11 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
+     * later - add differentiation between the valley and in beyond the valley locations
     */
+    return queryInterface.bulkInsert('Locations',[
+      {name:'Pelican Town'},{name:'Cindersap Forest'},{name:}
+    ])
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +25,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    return queryInterface.bulkDelete('Locations', null)
   }
 };

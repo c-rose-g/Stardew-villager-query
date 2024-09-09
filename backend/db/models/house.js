0b81b1a529d63c77d1924f01e7c6d8a876b3b303
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-08 21:35:37 -0700

    Update House table

--- backend/db/models/house.js
+++ backend/db/models/house.js
@@ -1,41 +1,42 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class House extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      House.belongsTo(models.Location, {foreignKey:'locationId'});
-    }
-  }
-  House.init({
-    id:{
-      type:DataTypes.INTEGER,
-      primaryKey:true,
-      allowNull:false,
-    },
-    name: {
-      type:DataTypes.STRING,
-      allowNull:false,
-    },
-    locationId: {
-      type:DataTypes.INTEGER,
-      references:{
-        key:'id',
-        model:'Locations'
-      },
-      allowNull:false,
-
-    }
-  }, {
-    sequelize,
-    modelName: 'House',
-  });
-  return House;
+	class House extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			House.belongsTo(models.Location, { foreignKey: "locationId" });
+			House.hasMany(models.Villager, { foreignKey: "buildingId" });
+		}
+	}
+	House.init(
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				allowNull: false,
+			},
+			address: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			locationId: {
+				type: DataTypes.INTEGER,
+				references: {
+					key: "id",
+					model: "Locations",
+				},
+				allowNull: false,
+			},
+		},
+		{
+			sequelize,
+			modelName: "House",
+		}
+	);
+	return House;
 };

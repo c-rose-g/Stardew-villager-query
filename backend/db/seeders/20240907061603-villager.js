8312587c2fc28c226950afa6506a63101fbc33b1
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-08 23:36:58 -0700

    Update Villager seeders

--- backend/db/seeders/20240907061603-villager.js
+++ backend/db/seeders/20240907061603-villager.js
@@ -1,62 +1,62 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-    return queryInterface.bulkInsert('Villagers',[
-      {name:'Abigail',sex:'Female',marriage:true},
-      {name:'Alex',sex:'Male',marriage:true},
-      {name:'Caroline',sex:'Female',marriage:false},
-      {name:'Clint',sex:'Male',marriage:true},
-      {name:'Demetrius',sex:'Male',marriage:false},
-      {name:'Drawf',sex:'Male',marriage:false},
-      {name:'Emily',sex:'Female',marriage:true},
-      {name:'Elliot',sex:'Male',marriage:true},
-      {name:'Evelyn',sex:'Female',marriage:false},
-      {name:'George',sex:'Male',marriage:false},
-      {name:'Gus',sex:'Male',marriage:false},
-      {name:'Haley',sex:'Female',marriage:true},
-      {name:'Harvey',sex:'Male',marriage:true},
-      {name:'Jas',sex:'Female',marriage:false},
-      {name:'Jodi',sex:'Female',marriage:false},
-      {name:'Kent',sex:'Male',marriage:false},
-      {name:'Krobus',sex:'Male',marriage:false},
-      {name:'Leah',sex:'Female',marriage:true},
-      {name:'Leo',sex:'Male',marriage:false},
-      {name:'Lewis',sex:'Male',marriage:false},
-      {name:'Linus',sex:'Male',marriage:false},
-      {name:'Marnie',sex:'Female',marriage:false},
-      {name:'Maru',sex:'Female',marriage:true},
-      {name:'Pam',sex:'Female',marriage:false},
-      {name:'Penny',sex:'Female',marriage:true},
-      {name:'Pierre',sex:'Female',marriage:false},
-      {name:'Robin',sex:'Female',marriage:false},
-      {name:'Sam',sex:'Male',marriage:true},
-      {name:'Sandy',sex:'Female',marriage:false},
-      {name:'Sebastian',sex:'Male',marriage:true},
-      {name:'Shane',sex:'Male',marriage:true},
-      {name:'Vincent',sex:'Male',marriage:false},
-      {name:'Willy',sex:'Male',marriage:false},
-      {name:'Wizard',sex:'Male',marriage:false}
-    ]);
-  },
+	async up(queryInterface, Sequelize) {
+		/**
+		 * Add seed commands here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkInsert('People', [{
+		 *   name: 'John Doe',
+		 *   isBetaMember: false, houseId:
+		 * }], {});
+		 */
+		return queryInterface.bulkInsert("Villagers", [
+			{ name: "Abigail", sex: "Female", marriage: true, buildingId: 6 },
+			{ name: "Alex", sex: "Male", marriage: true, houseId: 1 },
+			{ name: "Caroline", sex: "Female", marriage: false, buildingId: 6 },
+			{ name: "Clint", sex: "Male", marriage: true, buildingId: 1 },
+			{ name: "Demetrius", sex: "Male", marriage: false, houseId: 3 },
+			{ name: "Drawf", sex: "Male", marriage: false, houseId: 7 },
+			{ name: "Emily", sex: "Female", marriage: true, houseId: 4 },
+			{ name: "Elliot", sex: "Male", marriage: true, houseId: 10 },
+			{ name: "Evelyn", sex: "Female", marriage: false, houseId: 1 },
+			{ name: "George", sex: "Male", marriage: false, houseId: 1 },
+			{ name: "Gus", sex: "Male", marriage: false, buildingId: 7 },
+			{ name: "Haley", sex: "Female", marriage: true, houseId: 4 },
+			{ name: "Harvey", sex: "Male", marriage: true, buildingId: 3 },
+			{ name: "Jas", sex: "Female", marriage: false, buildingId: 9 },
+			{ name: "Jodi", sex: "Female", marriage: false, houseId: 2 },
+			{ name: "Kent", sex: "Male", marriage: false, houseId: 2 },
+			{ name: "Krobus", sex: "Male", marriage: false, buildingId: 8 },
+			{ name: "Leah", sex: "Female", marriage: true, houseId: 5 },
+			{ name: "Leo", sex: "Male", marriage: false, houseId: 12 },
+			{ name: "Lewis", sex: "Male", marriage: false, houseId: 8 },
+			{ name: "Linus", sex: "Male", marriage: false, houseId: 9 },
+			{ name: "Marnie", sex: "Female", marriage: false, buildingId: 9 },
+			{ name: "Maru", sex: "Female", marriage: true, houseId: 3 },
+			{ name: "Pam", sex: "Female", marriage: false, houseId: 6 },
+			{ name: "Penny", sex: "Female", marriage: true, houseId: 6 },
+			{ name: "Pierre", sex: "Female", marriage: false, buildingId: 6 },
+			{ name: "Robin", sex: "Female", marriage: false, houseId: 3 },
+			{ name: "Sam", sex: "Male", marriage: true, houseId: 2 },
+			{ name: "Sandy", sex: "Female", marriage: false, buildingId: 25 },
+			{ name: "Sebastian", sex: "Male", marriage: true, houseId: 3 },
+			{ name: "Shane", sex: "Male", marriage: true, buildingId: 9 },
+			{ name: "Vincent", sex: "Male", marriage: false, houseId: 2 },
+			{ name: "Willy", sex: "Male", marriage: false, buildingId: 14 },
+			{ name: "Wizard", sex: "Male", marriage: false, buildingId: 13 },
+		]);
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    return queryInterface.bulkDelete('Villagers',null)
-  }
+	async down(queryInterface, Sequelize) {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
+		return queryInterface.bulkDelete("Villagers", null);
+	},
 };

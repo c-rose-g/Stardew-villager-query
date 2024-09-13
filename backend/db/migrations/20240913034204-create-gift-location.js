bbf4a9399436b4f402251f61e5b687c8bc755aeb
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-12 20:43:58 -0700

    Create Gift Location through table

---
+++ backend/db/migrations/20240913034204-create-gift-location.js
@@ -0,0 +1,26 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Gift_Locations', {
+      
+      giftId: {
+        type: Sequelize.INTEGER
+      },
+      locationId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Gift_Locations');
+  }
+};
---
+++ backend/db/models/gift_location.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+	class Gift_Location extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Gift_Location.init(
+		{
+			giftId: { type: DataTypes.INTEGER, allowNull:false, references:{
+        model:'Gifts',
+        key:'id'
+      } },
+			locationId: { type: DataTypes.INTEGER, allowNull },
+		},
+		{
+			sequelize,
+			modelName: "Gift_Location",
+			timestamps: false,
+			primaryKey: ["giftId", "locationId"],
+		}
+	);
+	return Gift_Location;
+};

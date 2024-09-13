eae2a76cf74392ee81b401b60238895e1da67320
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-13 00:14:58 -0700

    Update gift building seeder

--- backend/db/seeders/20240913034848-gift-building.js
+++ backend/db/seeders/20240913034848-gift-building.js
@@ -1,25 +1,48 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+	async up(queryInterface, Sequelize) {
+		/**
+		 * Add seed commands here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkInsert('People', [{
+		 *   name: 'John Doe',
+		 *   isBetaMember: false
+		 * }], {});
+		 */
+		return queryInterface.bulkInsert("Gift_Buildings", [
+			{ giftId: 1, buildingId: null },
+			{ giftId: 2, buildingId: null },
+			{ giftId: 3, buildingId: null },
+			{ giftId: 4, buildingId: null },
+			{ giftId: 5, buildingId: null },
+			{ giftId: 6, buildingId: null },
+			{ giftId: 7, buildingId: null },
+			{ giftId: 8, buildingId: null },
+			{ giftId: 9, buildingId: null },
+			{ giftId: 10, buildingId: null },
+			{ giftId: 11, buildingId: null },
+			{ giftId: 12, buildingId: null },
+			{ giftId: 13, buildingId: null },
+			{ giftId: 14, buildingId: null },
+			{ giftId: 15, buildingId: null },
+			{ giftId: 16, buildingId: null },
+			{ giftId: 17, buildingId: 6 },
+			{ giftId: 18, buildingId: 6 },
+			{ giftId: 19, buildingId: 6 },
+			{ giftId: 20, buildingId: 6 },
+		]);
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+	async down(queryInterface, Sequelize) {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
+		return queryInterface.bulkDelete("Gift_Buildings");
+	},
 };

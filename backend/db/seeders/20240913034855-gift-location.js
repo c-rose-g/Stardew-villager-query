af3c5dc5b60871e66b9ce226894b90b73211507a
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-13 00:14:50 -0700

    Update gift location seeder

--- backend/db/seeders/20240913034855-gift-location.js
+++ backend/db/seeders/20240913034855-gift-location.js
@@ -1,25 +1,48 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+	async up(queryInterface, Sequelize) {
+		/**
+		 * Add seed commands here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkInsert('People', [{
+		 *   name: 'John Doe',
+		 *   isBetaMember: false
+		 * }], {});
+		 */
+		return queryInterface.bulkInsert("Gift_Locations", [
+			{ giftid: 1, locationId: 6 },
+			{ giftid: 2, locationId: 6 },
+			{ giftid: 3, locationId: 6 },
+			{ giftid: 4, locationId: 6 },
+			{ giftid: 5, locationId: 6 },
+			{ giftid: 6, locationId: 6 },
+			{ giftid: 7, locationId: 6 },
+			{ giftid: 8, locationId: 6 },
+			{ giftid: 9, locationId: 6 },
+			{ giftid: 10, locationId: 6 },
+			{ giftid: 11, locationId: 6 },
+			{ giftid: 12, locationId: 6 },
+			{ giftid: 13, locationId: 6 },
+			{ giftid: 14, locationId: 6 },
+			{ giftid: 15, locationId: 6 },
+			{ giftid: 16, locationId: 6 },
+			{ giftid: 17, locationId: 1 },
+			{ giftid: 18, locationId: 1 },
+			{ giftid: 19, locationId: 1 },
+			{ giftid: 20, locationId: 1 },
+		]);
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+	async down(queryInterface, Sequelize) {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
+		return queryInterface.bulkDelete("Gift_Locations");
+	},
 };

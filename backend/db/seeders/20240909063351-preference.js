5d47ec5c32715c2c00819f62fca1c483eea86613
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-12 19:34:49 -0700

    Update Preference seeder

--- backend/db/seeders/20240909063351-preference.js
+++ backend/db/seeders/20240909063351-preference.js
@@ -1,38 +1,38 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-    return queryInterface.bulkInsert('Preferences',[
-      {name:'Loves'},
-      {name:'Likes'},
-      {name:'Neutrals'},
-      {name:'Dislikes'},
-      {name:'Hates'},
-      {name:'Universal Loves'},
-      {name:'Universal Likes'},
-      {name:'Universal Neutrals'},
-      {name:'Universal Dislikes'},
-      {name:'Universal Hates'}
-    ])
-  },
+	async up(queryInterface, Sequelize) {
+		/**
+		 * Add seed commands here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkInsert('People', [{
+		 *   name: 'John Doe',
+		 *   isBetaMember: false
+		 * }], {});
+		 */
+		return queryInterface.bulkInsert("Preferences", [
+			{ name: "Loves" },
+			{ name: "Likes" },
+			{ name: "Neutrals" },
+			{ name: "Dislikes" },
+			{ name: "Hates" },
+			{ name: "Universal Loves" },
+			{ name: "Universal Likes" },
+			{ name: "Universal Neutrals" },
+			{ name: "Universal Dislikes" },
+			{ name: "Universal Hates" },
+		]);
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    return queryInterface.bulkDelete('Preferences')
-  }
+	async down(queryInterface, Sequelize) {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
+		return queryInterface.bulkDelete("Preferences", null);
+	},
 };

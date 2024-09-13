01669ee87bb7cf3b1ae9131a5f012200d54f9189
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-12 20:05:15 -0700

    Update gift migration file
    I removed categoryId, locationId, buildingId, and seasonId.

--- backend/db/migrations/20240907001033-create-gift.js
+++ backend/db/migrations/20240907001033-create-gift.js
@@ -12,46 +12,6 @@ module.exports = {
 				type: Sequelize.STRING,
 				allowNull: false,
 			},
-			categoryId: {
-				type: Sequelize.INTEGER,
-				references: {
-					key: "id",
-					model: "Categories",
-				},
-				allowNull: false,
-				onUpdate: "CASCADE",
-				onDelete: "SET NULL",
-			},
-			locationId: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				references: {
-					key: "id",
-					model: "Locations",
-				},
-				onUpdate: "CASCADE",
-				onDelete: "SET NULL",
-			},
-			seasonId: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				references: {
-					key: "id",
-					model: "Seasons",
-				},
-				onUpdate: "CASCADE",
-				onDelete: "SET NULL",
-			},
-			buildingId: {
-				type: Sequelize.INTEGER,
-				allowNull: true,
-				references: {
-					key: "id",
-					model: "Buildings",
-				},
-				onUpdate: "CASCADE",
-				onDelete: "SET NULL",
-			},
 			createdAt: {
 				allowNull: false,
 				type: Sequelize.DATE,

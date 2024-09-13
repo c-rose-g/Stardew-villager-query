ae2b6a2fdde8a7c38af3e39c6be75841ec77b1dd
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-12 20:24:09 -0700

    Update gift_category through table

--- backend/db/migrations/20240913024640-create-gift-category.js
+++ backend/db/migrations/20240913024640-create-gift-category.js
@@ -1,31 +1,33 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('gift_categories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      giftId: {
-        type: Sequelize.INTEGER
-      },
-      categoryId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('gift_categories');
-  }
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("Gift_Categories", {
+			giftId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: "Gifts",
+					key: "id",
+				},
+			},
+			categoryId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: "Categories",
+					key: "id",
+				},
+			},
+		});
+		// Add the composite primary key
+		await queryInterface.addConstraint("Gift_Categories", {
+			fields: ["giftId", "categoryId"],
+			type: "primary key",
+			name: "pkGiftCategory",
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("Gift_Categories");
+	},
 };
--- backend/db/models/gift_category.js
+++ backend/db/models/gift_category.js
@@ -1,24 +1,35 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class gift_category extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  gift_category.init({
-    giftId: DataTypes.INTEGER,
-    categoryId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'gift_category',
-  });
-  return gift_category;
+	class Gift_Category extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Gift_Category.init(
+		{
+			giftId: {
+				type: DataTypes.INTEGER,
+				references: { model: "Gifts", key: "id" },
+				allowNull: false,
+			},
+			categoryId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				references: { model: "Categories", key: "id" },
+			},
+		},
+		{
+			sequelize,
+			modelName: "Gift_Category",
+			timestamps: false,
+			primaryKey: ["giftId", "categoryId"],
+		}
+	);
+	return Gift_Category;
 };

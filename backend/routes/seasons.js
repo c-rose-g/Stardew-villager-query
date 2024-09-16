7b4bdaf36df5f845244b937aeac47982ac972f37
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-16 00:00:39 -0700

    Rename seasons.js

--- backend/routes/seasons.js
+++ backend/routes/seasons.js
@@ -2,4 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { op } = require("sequelize");
 
+// GET ALL SEASONS
+
+// GET ONE SEASON BY id
+
 module.exports = router;

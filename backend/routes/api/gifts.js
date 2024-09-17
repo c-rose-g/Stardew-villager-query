03028b4a4b72a01bda550fa754bb6410cedc35df
Cindy Guzman <cindyrose.go@gmail.com>
2024-09-16 17:43:54 -0700

    Update gifts.js

--- backend/routes/api/gifts.js
+++ backend/routes/api/gifts.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { Gift, Gift_Building, Gift_Category, Gift_Location, Gift_Season, Villager_Gift} = require('../../db/models')
 const { op } = require("sequelize");
-
-const { gift_building, gift_category, gift_location, gift_season} = require('../../db/models')
 // *** Search gifts by building, category, location, season, and villager
 
-
+router.get('/all', async (req, res) =>{
+  /*
+  return all gifts
+   */
+  const gifts = await Gift.findAll()
+  res.status(200).send({Gift:gifts})
+})
 
 module.exports = router;

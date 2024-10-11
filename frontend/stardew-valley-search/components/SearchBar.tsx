685cc5bd7827acbda8e84e496028fe7b9fd0051f
Cindy Guzman <cindyrose.go@gmail.com>
2024-10-11 02:12:29 -0700

    Update SearchBar.tsx
    I have rendered query for villager, but I plan on creating different components for each type of search, and importing those components to SearchBar and conditionally render search components

--- frontend/stardew-valley-search/components/SearchBar.tsx
+++ frontend/stardew-valley-search/components/SearchBar.tsx
@@ -1,23 +1,52 @@
 import React, { useState } from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
-
+import { View, TextInput, StyleSheet, Button, Text } from 'react-native';
+import { CollapsibleResults } from '@/components/CollapsibleResults';
+import { useSearch } from '@/hooks/useSearch';
 interface SearchBarProps {
   onSearch: (query: string) => void;
 }
 
 export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const [query, setQuery] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  // destructure the hook
+  const { search, results, loading, error } = useSearch();
 
-  // 
+  const handleSearch = async() =>{
+    await search(query);
+    setSubmitted(true);
+
+  }
+  console.log('RESULTS >>', results)
   return (
     <View style={styles.container}>
       <TextInput
-        style={styles.input}
-        placeholder="Search..."
         value={query}
+        placeholder="Search..."
+        style={styles.input}
         onChangeText={setQuery}
         onSubmitEditing={() => onSearch(query)}
+
       />
+      <Button title='Search' onPress={handleSearch} />
+      {loading && !results && <Text>Loading...</Text>}
+      {submitted && !loading && results.length > 0 && (
+        <View>
+          <Text>Results:</Text>
+    
+            {results.map((result: { name: string, schedule: object, buildingId: number, marriage: boolean, sex: string }, index) => (
+              result.name ? ( // Check if name is defined before rendering
+                <Text key={index}>
+                  {`${result.name} is a villager.`}{"\n"}
+                  {`${result.sex === 'Female' ? 'She' : 'He'} lives in ${result.buildingId}.`}{"\n"}
+                  {`They ${result.marriage ? 'are open' : 'are not open'} to marriage.`}
+                  </Text>
+                  ) : null // Avoid rendering when name is undefined
+                  ))}
+                  </View>
+        )}
+      {submitted && !loading && !results.length && error && <Text style={{ color: 'red' }}>{error}</Text>}
+      {/* <CollapsibleResults results={results} /> */}
     </View>
   );
 };
@@ -40,3 +69,5 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
 });
+
+// export default Search

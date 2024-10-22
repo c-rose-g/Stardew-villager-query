f36503fa0b99570c962c2c35ca57c7a1aac4d7ba
Cindy Guzman <cindyrose.go@gmail.com>
2024-10-21 19:26:16 -0700

    Update SearchBar.tsx
    I am conditionally rendering components for the search bar based on the model provided.

--- frontend/stardew-valley-search/components/SearchBar.tsx
+++ frontend/stardew-valley-search/components/SearchBar.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { View, TextInput, StyleSheet, Button, Text, ScrollView } from 'react-native';
 import { CollapsibleResults } from '@/components/CollapsibleResults';
+import { VillagerResults } from '../components/VillagerResults';
+
 import { useSearch } from '@/hooks/useSearch';
 interface SearchBarProps {
   onSearch: (query: string  | null ) => void;
@@ -10,8 +12,9 @@ export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const [query, setQuery] = useState('');
   const [submitted, setSubmitted] = useState(false);
   const [searchError, setSearchError] = useState<string | null>(null); // State to track search errors
+
   // destructure the hook
-  const { search, results, loading, error } = useSearch();
+  const { search, results, loading, error, model } = useSearch();
 
   const handleSearch = async() =>{
     const searchResults = await search(query);
@@ -23,7 +26,7 @@ export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
       setSearchError(null);
     }
   }
-  // console.log('RESULTS >>', results)
+  // console.log('this is model:', model)
   return (
     <>
     <View style={styles.container}>
@@ -39,42 +42,18 @@ export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
       </View>
       {loading && !results && <Text>Loading...</Text>}
       {submitted && !loading && results.length > 0 && (
-      <ScrollView style={styles.resultsContainer}>
+        <Text>{model === 'villagers' ? (<VillagerResults results={results} />)
+        : model === 'gifts' ? 'gifts'
+        : model === 'schedules' ? 'schedules' : 'no'
+        }</Text>
 
-        {results.map((result: { name: string, Schedule: Array<{seasonId:number, description:string, startLocationId: number, endLocationId:number, startBuildingId:number, endBuildingId:number, time:number, weekday:string | null, weather:string | null, isFestival:boolean}>, buildingId: number, houseId:number, marriage: boolean, sex: string, Gifts: Array<{ name: string, Villager_Gift: { preferenceId: number } }> | null }, index) => (
-          result.name ? (
-          <View key={index}>
-            <Text>
-              {`${result.name} is a villager.`}{"\n"}
-              {`${result.sex === 'Female' ? 'She' : 'He'} lives in ${result.buildingId? result.buildingId: result.houseId}.`}{"\n"}
-              {`They ${result.marriage ? 'are open' : 'are not open'} to marriage.`}
-            </Text>
-            <Text>{`Want to give ${result.name} a gift?`}</Text>
-            {result.Gifts ? (
-              result.Gifts.map((gift, giftIndex) => (
-                <Text key={giftIndex}>
-                  {gift.Villager_Gift.preferenceId === 1 && `${gift.name} (Loved)`}
-                  {gift.Villager_Gift.preferenceId === 2 && `${gift.name} (Liked)`}
-                  {gift.Villager_Gift.preferenceId === 3 && `${gift.name} (Neutral)`}
-                  {gift.Villager_Gift.preferenceId === 4 && `${gift.name} (Disliked)`}
-                  {gift.Villager_Gift.preferenceId === 5 && `${gift.name} (Hated)`}
-                </Text>
-              ))
-            ) : (
-            <Text>{result.name} has no gifts they want.</Text>
-          )}
-        </View>
-      ) : null
-    ))}
-      </ScrollView>
-        )}
+      )}
       {submitted && !loading && !results.length && error && <Text style={{ color: 'red' }}>{error}</Text>}
       {/* <CollapsibleResults results={results} /> */}
 
     </>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     padding: 10,

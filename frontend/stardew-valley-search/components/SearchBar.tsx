5ea6bb99568934f5bd763386258b1dad5f2a0d22
Cindy Guzman <cindyrose.go@gmail.com>
2024-10-29 15:00:45 -0700

    Update searchBar.tsx

--- frontend/stardew-valley-search/components/SearchBar.tsx
+++ frontend/stardew-valley-search/components/SearchBar.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { SafeAreaView, View, TextInput, StyleSheet, Button, Text, Animated, Easing, ScrollView } from 'react-native';
-import { VillagerResults } from './SearchVillagers';
+import React, { useEffect, useState, useRef } from 'react';
+import { SafeAreaView, View, TextInput, StyleSheet, Button, Text, Animated, Easing, ScrollView, FlatList } from 'react-native';
+import { SearchVillagers } from './SearchVillagers';
+import { SearchGifts } from './SearchGifts';
 import type { EasingFunction } from 'react-native';
 
 import { useSearch } from '@/hooks/useSearch';
@@ -10,11 +11,28 @@ type SearchBarProps = {
 }
 
 export const SearchBar = ({ onSearch }: SearchBarProps) => {
+
   const opacity = useState(new Animated.Value(0))[0];
   const height = useState(new Animated.Value(0))[0];
+  const searchBarAnim = useState(new Animated.Value(0))[0];
+
   const [query, setQuery] = useState('');
   const [submitted, setSubmitted] = useState(false);
   const [searchError, setSearchError] = useState<string | null>(null);
+
+  useEffect( () => {
+
+    const startSearchBarAnimate = () =>{
+      Animated.timing( searchBarAnim,{
+        toValue:1,
+        duration:100,
+        delay:10,
+        useNativeDriver:true,
+      }).start();
+    };
+
+    startSearchBarAnimate();
+  },[searchBarAnim]);
 
   const animate = (easing: EasingFunction) => {
     Animated.timing(opacity, {
@@ -25,12 +43,13 @@ export const SearchBar = ({ onSearch }: SearchBarProps) => {
     }).start();
 
     Animated.timing(height, {
-      toValue: 200, // Adjust based on desired final height of the results container
+      toValue: 450, // Adjust based on desired final height of the results container
       duration: 400,
       easing,
       useNativeDriver: false, // height animation cannot use the native driver
     }).start();
   };
+
 
   const { search, results, error, model } = useSearch();
 
@@ -46,43 +65,53 @@ export const SearchBar = ({ onSearch }: SearchBarProps) => {
     }
   };
 
-  console.log('this is model', model)
+
+
+  // console.log('this is model', model)
   const animatedStyles = {
     opacity,
     height,
   };
 
   return (
-    <SafeAreaView>
-      <View style={styles.searchContainer}>
-        <TextInput
-          value={query}
-          placeholder="Search..."
-          style={styles.input}
-          onChangeText={setQuery}
-          onSubmitEditing={() => onSearch(query)}
-        />
-        <Button title="Search" onPress={handleSearch} />
-      </View>
-      {submitted && results.length > 0 && (
-        <Animated.View style={[styles.resultsContainer, animatedStyles]}>
-          <ScrollView>
-          <Text>
-            { model === 'villagers' ? (<VillagerResults results={results} />)
-            : model === 'gifts' ? 'gifts results'
-            : model === 'schedules' ? 'schedules results' : 'no'}
-          </Text>
-          </ScrollView>
-        </Animated.View>
-      )}
-      {submitted && !results.length && searchError && (
-        <Text style={{ color: 'red' }}>{searchError}</Text>
-      )}
+    <SafeAreaView style={styles.SafeAreaViewContainer}>
+      {/* <Animated.View > */}
+
+        <View style={styles.searchContainer}>
+          <TextInput
+            value={query}
+            placeholder="Search..."
+            style={styles.input}
+            onChangeText={setQuery}
+            onSubmitEditing={() => onSearch(query)}
+            />
+          <Button title="Search" onPress={handleSearch} />
+        </View>
+        {submitted && results.length > 0 && (
+          <Animated.View style={[styles.resultsContainer, animatedStyles]}>
+            <View>
+              <Text>
+                { model === 'villagers' ? (<SearchVillagers results={results} />)
+                : model === 'gifts' ? (<SearchGifts results={results}/>)
+                : model === 'schedules' ? 'schedules results' : 'no'}
+              </Text>
+            </View>
+          </Animated.View>
+        )}
+        {submitted && !results.length && searchError && (
+          <Text style={{ color: 'red' }}>{searchError}</Text>
+        )}
+
+      {/* </Animated.View> */}
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  SafeAreaViewContainer:{
+    // borderWidth:1,
+    // borderColor:'yellow',
+  },
   input: {
     height: 40,
     borderColor: '#ccc',
@@ -94,14 +123,15 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: '#ffffff',
     borderRadius: 5,
-    shadowColor: '#000',
-    shadowOpacity: 0.1,
-    shadowRadius: 5,
+    // shadowColor: '#fff',
+    // shadowOpacity: 0.1,
+    // shadowRadius: 5,
   },
   resultsContainer: {
     marginTop: 10,
+    padding: 20,
     backgroundColor: '#fff',
     borderRadius: 5,
-    overflow: 'hidden', // Ensures smooth rounded corners during animation
+    // overflow: 'visible', // Ensures smooth rounded corners during animation
   },
 });

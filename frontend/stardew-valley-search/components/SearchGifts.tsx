b19969b6a4b0c5dd3016212f49ad86d682886d04
Cindy Guzman <cindyrose.go@gmail.com>
2024-10-31 16:37:40 -0700

    Update SearchGifts.tsx
    Upd

--- frontend/stardew-valley-search/components/SearchGifts.tsx
+++ frontend/stardew-valley-search/components/SearchGifts.tsx
@@ -1,29 +1,67 @@
 import React, {useState} from "react";
 import { SafeAreaView, View, ScrollView, StyleSheet, Text } from "react-native";
-import Gifts from '../../../backend/db/models/gift'
-import Preferences from '../../../backend/db/models/preference'
-import villager from "../../../backend/db/models/villager";
-import preference from "../../../backend/db/models/preference";
-import gift from "../../../backend/db/models/gift";
-import { SearchVillagers } from "./SearchVillagers";
+import { IndexPath, Layout, Select, SelectItem } from '@ui-kitten/components';
 
 type ComponentProps = { results : Array <any>}
 
-export const SearchGifts = ({results}:ComponentProps)=> {
+// interface IndexPath{
+//   row: number
+// }
 
-  // const 
+export const SearchGifts = ({results}:ComponentProps)=> {
+  const [selectedIdx, setSelectedIdx] = useState<IndexPath | IndexPath[]>(new IndexPath(0));
+
+  const villagerPreference = (idx:any) =>{
+    setSelectedIdx(idx)
+    
+  }
+
+  console.log('this is selectedIdx =>', selectedIdx)
   return (
     <ScrollView style={styles.container}>
-      {results.map((result:{id:number, name:string, VillagerGifts: Array<{villagerId:number, giftId:number, preferenceId:number, Preference:{name:string}}> | null}, index) => (
+      {results.map((result:{
+        id:number,
+        name:string,
+        VillagerGifts: Array<{
+          villagerId:number,
+          giftId:number,
+          preferenceId:number,
+          Villager: { name: string },
+          Preference: { name: string }
+        }> | null
+      }, index) => (
         result.name ? (
         <View key={index}>
           <Text style={styles.title}>
             {`${result.name}`}
           </Text>
           <Text style={styles.resultsText}>
-          {result.VillagerGifts?.length ? (result.VillagerGifts.map(vg =>
-            `this is villager id: ${vg.villagerId}, this is gift id: ${vg.giftId}, this is preference id:${vg.preferenceId}`).join(', ')) : null}
+            {result.VillagerGifts?.length ? result.VillagerGifts.map(vg => (
+            // create a drop down list using Select from react native UI kitten
+            // that allows me to toggle between Loves, Likes, Neautral, Dislikes, Hates and shows the villager with the gift preference
+            // `Villagers [select] this gift`
+            <View key={index}>
+            <Layout level="1">
+              <Text>Villagers that
+                <Select
+                selectedIndex={selectedIdx}
+                onSelect={idx => setSelectedIdx(idx)}
+                >
+                <SelectItem title='loves'/>
+                <SelectItem title='likes'/>
+                <SelectItem title='is neutral about'/>
+                <SelectItem title='dislikes'/>
+                <SelectItem title='hates'/>
+              </Select> it: {}
+                  </Text>
+            </Layout>
 
+                </View>
+          )
+            )
+
+
+            : 'No villager preferences for this gift'}
             </Text>
         </View>
         ):(<Text>there is nothing in Gifts</Text>)
@@ -35,6 +73,9 @@ export const SearchGifts = ({results}:ComponentProps)=> {
 const styles = StyleSheet.create({
   container:{
     borderWidth:1,
+  },
+  selectContainer:{
+    minHeight:128
   },
   title:{
     fontWeight:"bold",

3106ecac2fa3f1d83aa1432315bb37f3a0000068
Cindy Guzman <cindyrose.go@gmail.com>
2024-12-12 18:40:45 -0800

    Update gift_preference model

--- backend/db/models/gift_preference.js
+++ backend/db/models/gift_preference.js
@@ -1,43 +1,46 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Gift_Preference extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      Gift_Preference.belongsTo(models.Gift);
-      Gift_Preference.belongsTo(models.Preference)
-    }
-  }
-  Gift_Preference.init({
-    giftId: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      references: {
-        model: 'Gifts',
-        key: 'id'
-      },
-
-    },
-    preferenceId: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      references: {
-        model: 'Preferences',
-        key: 'id'
-      },
-
-    }
-  }, {
-    sequelize,
-    modelName: 'Gift_Preference',
-    timestamps:false,
-  });
-  return Gift_Preference;
+	class Gift_Preference extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			Gift_Preference.belongsTo(models.Gift, {
+				foreignKey: "giftId",
+			});
+			Gift_Preference.belongsTo(models.Preference, {
+				foreignKey: "preferenceId",
+			});
+		}
+	}
+	Gift_Preference.init(
+		{
+			giftId: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				references: {
+					model: "Gifts",
+					key: "id",
+				},
+			},
+			preferenceId: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				references: {
+					model: "Preferences",
+					key: "id",
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: "Gift_Preference",
+			timestamps: false,
+		}
+	);
+	return Gift_Preference;
 };

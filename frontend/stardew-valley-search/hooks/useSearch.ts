4aebe56dd8ecd44063de9615eed79cb838bbdc11
Cindy Guzman <cindyrose.go@gmail.com>
2024-12-14 16:49:48 -0800

    Update useSearch.ts

--- frontend/stardew-valley-search/hooks/useSearch.ts
+++ frontend/stardew-valley-search/hooks/useSearch.ts
@@ -12,7 +12,8 @@ export const useSearch = () => {
     try {
       // remove whitespace around the query
       const sanitizedQuery = query.trim()
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/search?query=${sanitizedQuery}`);
+      // find mobile in .env
+      const response = await fetch(`http://localhost:8000/search?query=${sanitizedQuery}`);
 
       if (!response.ok) {
 

dd2331873b709799f2011eaa844db3bd60ebd5c9
Cindy Guzman <cindyrose.go@gmail.com>
2024-12-20 22:00:06 -0800

    Update index.tsx

--- frontend/stardew-valley-search/app/(tabs)/index.tsx
+++ frontend/stardew-valley-search/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { Dimensions, SafeAreaView, Animated, ImageBackground, Image, StyleSheet, Platform, View } from 'react-native';
 import { SearchBar } from '@/components/SearchBar'
 import { StatusBar } from 'expo-status-bar';
@@ -15,23 +15,25 @@ type ParallaxScrollViewProps = {
 export default function HomeScreen ({renderBackground, renderForeground, children}: ParallaxScrollViewProps) {
   const fadeAnim = useRef(new Animated.Value(0)).current;
   const { width, height } = Dimensions.get('window');
-  const colorScheme = useColorScheme();
-  const themeTextStyle = colorScheme === 'light' ? Colors.light : Colors.dark;
-  const themeContainerStyle = colorScheme === 'light' ? Colors.light : Colors.dark;
+  const systemColorScheme = useColorScheme();
+  const [theme, setTheme] = useState(systemColorScheme === 'dark' ? Colors.dark : Colors.light);
+  const isDarkMode = theme === Colors.dark;
+  const statusBackgroundColor = isDarkMode ? Colors.dark.statusBarBackgroundColor : Colors.light.statusBarBackgroundColor;
 
-  const container = {
-    flex: 1,
-  };
   const searchContainer = {
-    height: height,
+    height: height / 1.2,
     width: width,
     justifyContent: 'center' as 'center',
   };
 
-  const background = {
+  const imageBackground = {
     height:height,
+    flex:1,
   };
 
+  const statusBarBackgroundColor ={
+    backgroundColor: statusBackgroundColor,
+  }
   useEffect(() => {
     const startAnimation = () => {
       Animated.timing(fadeAnim, {
@@ -45,17 +47,16 @@ export default function HomeScreen ({renderBackground, renderForeground, childre
     startAnimation();
   }, [fadeAnim]);
 
-
   return (
-      <SafeAreaView style={[{ backgroundColor: themeContainerStyle.background }, {flex:1}]}>
+      <SafeAreaView  style={[statusBarBackgroundColor,{flex:1}]}>
         <StatusBar style="auto" />
-        <ImageBackground source={require('@/assets/images/index-bg.png')} style={background}>
-        <View style={container}>
-          {/* <Animated.View style={{ opacity: fadeAnim, flex: 2 }}> */}
+        <ImageBackground source={require('@/assets/images/index-bg.png')} style={imageBackground}>
+        <View>
+          <Animated.View style={{ opacity: fadeAnim, flex: 2 }}>
             <View style={searchContainer}>
               <SearchBar onSearch={() => {}} />
               </View>
-              {/* </Animated.View> */}
+              </Animated.View>
         </View>
 
           <View>{children}</View>

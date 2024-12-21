c5ac563842635bbf021d3cfaf70620d14ec6c375
Cindy Guzman <cindyrose.go@gmail.com>
2024-12-20 21:54:12 -0800

    Update Colors.ts

--- frontend/stardew-valley-search/constants/Colors.ts
+++ frontend/stardew-valley-search/constants/Colors.ts
@@ -3,24 +3,51 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
+const tintColorLight = '#40B5F6';
+const tabBackgroundColorLight = '#fff';
+const statusBarBackgroundColorLight = '#fff';
+const titleColorLight = "#000";
+const inputBorderColorLight = "#ccc";
+const inputTextColorLight = "#fff";
+const linkLight = '#073980';
+
 const tintColorDark = '#fff';
 const backgroundColorDark = '#101D60';
+const tabBackgroundColorDark = '#101D60';
+const statusBarBackgroundColorDark = '#101D60';
+const titleColorDark = "#ccc";
+const inputBorderColorDark = "#ccc";
+const inputTextColorDark = "#000";
+const linkDark = '#fff';
+
 export const Colors = {
   light: {
-    text: '#12195b',
+    text: '#000',
     background: '#fff',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
+    statusBarBackgroundColor:'#fff',
+    tabBackgroundColor: tabBackgroundColorLight,
+    titleColor:titleColorLight,
+    inputBorderColor: inputBorderColorLight,
+    inputTextColor:inputTextColorLight,
+    link:linkLight,
+
   },
   dark: {
-    text: '#ECEDEE',
+    text: '#69EDE5',
     background: backgroundColorDark,
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+    statusBarBackgroundColor:statusBarBackgroundColorDark,
+    tabBackgroundColor: tabBackgroundColorDark,
+    titleColor:titleColorDark,
+    inputBorderColor:inputBorderColorDark,
+    inputTextColor: inputTextColorDark,
+    link:linkDark,
   },
 };
